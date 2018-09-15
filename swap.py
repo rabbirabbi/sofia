a38facea20bb480d5872ec01ed70646c49decf4f
rabbirabbi <43239804+rabbirabbi@users.noreply.github.com>
2018-09-15 11:18:13 +0530

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,10 +1,15 @@
+#write a programme  to swap numbers 
+#Assign values 
 a=4
 b=5
 c=6
+# print the values
 print(a,b,c)
+# command to swap the numbers
 d=a
 a=b
 b=c
 c=d
+# print the numbers after swapping
 print("after swap")
 print(a,b,c)
