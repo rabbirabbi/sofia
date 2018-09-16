860be5c0544a467a0e0fca464c46b8b100437070
rabbirabbi <43239804+rabbirabbi@users.noreply.github.com>
2018-09-16 14:24:41 +0530

    Update grls.py

--- grls.py
+++ grls.py
@@ -1,8 +1,13 @@
+#write a programme to compare 2numbers (greater or lesser or equal) using if else statement
+#intake values of 2 numbers
+#using if else statement give the conditions 
+#execute the output
+
 a=int(input("enter value1"))
 b=int(input("enter value2"))
 if(a>b):
 	print("a is greater")
 elif(b>a):
-    print("b is greater")
+        print("b is greater")
 else:
-   print("a equal to b")    	
+    print("a equal to b")    	
