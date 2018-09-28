7db3a30ada6a0e4d96f8609650498abc69d6dcbf
rabbirabbi <43239804+rabbirabbi@users.noreply.github.com>
2018-09-28 16:21:26 +0530

    Add files via upload

---
+++ mathff.py
@@ -0,0 +1,15 @@
+a=int(input("value1:"))
+b=int(input("value2:"))
+def add():
+	return a+b
+def sub():
+	return a-b
+def mul():
+	return a*b
+def div():
+	return a/b
+
+print("addition of a and b",add())
+print("subraction of a and b",sub())
+print("multiplication of a and b",mul())
+print("division of a and b",div())
